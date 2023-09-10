8fd247a3910b4b0e49f9915067847f9f662bf204
Park Library <140351025+parklibrary@users.noreply.github.com>
2023-09-10 18:42:22 +0100

    Add files via upload

---
+++ js/bookambiance.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    const images = [
+        '01.JPG',
+        '02.JPG',
+        '03.JPG',
+    ];
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    document.documentElement.style.setProperty('--random-bg-url', `url('../../assets/backgrounds/${randomImage}')`);
+
+
+const audioFiles = [
+    '01.m4a',
+    '02.m4a',
+    '03.m4a',
+];
+const randomAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+document.getElementById('random-audio').setAttribute('src', `../../assets/audio/${randomAudio}`);
+};
+
